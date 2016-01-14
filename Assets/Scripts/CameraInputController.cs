2b7d99fa485d6c4f04999d6841a3556344cba3a6
Kaleb Lott <Kaleb Lott>
2016-01-14 07:09:23 -0700

    Commit changes from home.

---
+++ Assets/Scripts/CameraInputController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraInputController : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.LeftAlt) || Input.GetKeyDown(KeyCode.RightAlt)) {
+            GetComponent<FreeFlyCamera>().Active = !GetComponent<FreeFlyCamera>().Active;
+            GetComponent<SimpleSmoothMouseLook>().Active = !GetComponent<SimpleSmoothMouseLook>().Active;
+        }
+	}
+}
---
+++ Assets/Scripts/FreeFlyCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Camera/Free Fly Camera")]
+public class FreeFlyCamera : MonoBehaviour {
+
+    public float Speed = 0.3f;
+    public bool Active = true;
+
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetAxis("Vertical") != 0) {
+            transform.Translate(Vector3.forward * Speed * Input.GetAxis("Vertical"));
+        }
+        if (Input.GetAxis("Horizontal") != 0) {
+            transform.Translate(Vector3.right * Speed * Input.GetAxis("Horizontal"));
+        }
+    }
+}
