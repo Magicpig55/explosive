a45eac7f9369536a6c3f5f3ba913ac401d38d811
unknown <48513@747S10-07.nebo.local>
2016-02-22 10:14:01 -0700

    Added level selection and persistant menu stuff. TODO: disable buttons for levels that you haven't been to yet.

---
+++ Assets/Scripts/LevelButtonControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class LevelButtonControl : MonoBehaviour {
+
+	public LevelControl levelControl;
+	public Text text;
+	public int Level;
+	
+	void Update() {
+		text.text = string.Format ("Level {0}:{1}", Mathf.Floor ((Level - 1) / 5) + 1, ((Level - 1) % 5));
+		name = string.Format ("bt_{0}_{1}", Mathf.Floor ((Level - 1) / 5) + 1, ((Level - 1) % 5));
+	}
+
+	public void Click() {
+		Application.LoadLevel (Level);
+	}
+
+	void OnMouseDown() {
+		Click ();
+	}
+}
---
+++ Assets/Scripts/LevelControl.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+public class LevelControl : MonoBehaviour {
+
+	public void ChangeLevel(string scene) {
+		Application.LoadLevel (scene);
+	}
+	public void ChangeLevel(int scene) {
+		Application.LoadLevel (scene);
+	}
+	public void Restart() {
+		Application.LoadLevel (currentLevel);
+		escapeMenuOpen = false;
+		EscapeMenu.SetTrigger ("disable");
+	}
+
+	private int currentLevel = 0;
+	private WinConditions win;
+	private bool escapeMenuOpen = false;
+
+	public Animator EscapeMenu;
+	public Animator LevelMenu;
+	public Animator MainMenu;
+	public bool[] LevelComplete = new bool[10];
+
+	void Awake() {
+		DontDestroyOnLoad (transform.gameObject);
+		if (FindObjectsOfType(GetType()).Length > 1) {
+			DestroyImmediate(gameObject);
+		}
+	}
+	void OnLevelWasLoaded(int level) {
+		currentLevel = level;
+		win = Camera.main.GetComponent<WinConditions>();
+		EscapeMenu.SetTrigger ("disable");
+		LevelMenu.SetTrigger ("disable");
+		if (level == 0)
+			MainMenu.SetTrigger ("enable");
+		else 
+			MainMenu.SetTrigger ("disable");
+		escapeMenuOpen = false;
+	}
+	void Update() {
+		if (win != null) {
+			if (win.Won) {
+				LevelComplete[currentLevel + 1] = true;
+				Application.LoadLevel (currentLevel + 1);
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.Escape) && currentLevel != 0) {
+			if(escapeMenuOpen)
+				EscapeMenu.SetTrigger("disable");
+			else 
+				EscapeMenu.SetTrigger("enable");
+			escapeMenuOpen =! escapeMenuOpen;
+			Debug.Log("Oh boy!");
+		}
+	}
+}
